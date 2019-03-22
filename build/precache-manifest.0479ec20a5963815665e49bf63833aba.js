62914c356123b30ffe3cf92e1ea6566401700b63
JPatoliya <jiten1310@outlook.com>
2019-03-22 15:05:09 -0500

    build included in repo

---
+++ build/precache-manifest.0479ec20a5963815665e49bf63833aba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a75842ef8e51cfaf00df",
+    "url": "/static/js/main.0a7a4449.chunk.js"
+  },
+  {
+    "revision": "f0ef5ced837470b14686",
+    "url": "/static/js/2.7cb68033.chunk.js"
+  },
+  {
+    "revision": "a75842ef8e51cfaf00df",
+    "url": "/static/css/main.8c32f255.chunk.css"
+  },
+  {
+    "revision": "f0ef5ced837470b14686",
+    "url": "/static/css/2.4e387765.chunk.css"
+  },
+  {
+    "revision": "ee52e1d80623e79fb541b325619916ca",
+    "url": "/index.html"
+  }
+];
